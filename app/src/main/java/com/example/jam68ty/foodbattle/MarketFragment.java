4edef13d7da2a27cae681144d3553a99e7cd4866
yoyo861110 <yoyo861110@gmail.com>
2018-06-11 11:50:14 +0800

    團購市集（本日推薦連結團購內容）

--- app/src/main/java/com/example/jam68ty/foodbattle/MarketFragment.java
+++ app/src/main/java/com/example/jam68ty/foodbattle/MarketFragment.java
@@ -4,10 +4,13 @@ package com.example.jam68ty.foodbattle;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -30,4 +33,23 @@ public class MarketFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_market, container, false);
     }
+
+
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Button button = (Button) getView().findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                i.setClass(getActivity(), GroupitemActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+
+    }
+
+
 }
